53c05c873772810dccd2d8c579b0e1c75e00a2e9
ZeeQyu <mcmastasadventures@gmail.com>
2013-07-12 22:24:08 +0200

    Changed graphics.py to work with updated constants.py

--- src/graphics.py
+++ src/graphics.py
@@ -5,7 +5,7 @@ from constants import *
 class Graphics(object):
  
     def __init__(self, name):
-        self.image = pygame.image.load("res\\" + TILES[name][0])
+        self.image = pygame.image.load("res\\" + IMAGES[name][0])
     
     def get(self):
         return self.image
