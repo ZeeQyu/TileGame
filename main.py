d0ad0aec5c04ce4a2a13b711b083c4ba06058c1d
ZeeQyu <mcmastasadventures@gmail.com>
2013-07-12 22:23:39 +0200

    Added a player and moved map related functions to a separate file

--- main.py
+++ main.py
@@ -1,72 +1,42 @@
-import pygame, sys, os, Image
+import pygame, sys, os, Image, threading, time
 from pygame.locals import *
+
 sys.path.append(os.getcwd() + "\\src")
 
 from tiles import *
 from graphics import *
 from constants import *
-
-# Debug = 1: debug messages. Debug = 0: no debug messages.
-DEBUG = 1
-
-class InvalidColorException(Exception):
-    pass
-
-def get_map(map_name):
-    return Image.open("res\\" + map_name)
-
-def generate_map(map_image):
-    pixels = map_image.load()
-    map = []
-    width, height = map_image.size
-    for x in range(width):
-        map.append([])
+from player import *
+from map import *
         
-        if DEBUG: print "x:", x,
-        
-        for y in range(height):
-            pixel = pixels[x, y]
-            type = pixel_type(pixel, x, y)
-            
-            tile = Tile(type, x, y)
-            map[x].append(tile)
-            
-            if DEBUG: print "y:", y, pixel, type, "|",
-        if DEBUG: print
-    return map, width, height
-    
-def pixel_type(pixel, x, y):
-    for key in TILES:
-        # if the RGB value in TILES actually has a value
-        if TILES[key][1] != 0:
-            if pixel == TILES[key][1]:
-                return key
-    raise InvalidColorException("The pixel at x:", x, "y:", y, "in the map file is not a valid color. The RGB is", str(pixel))
-    
-def paint_map(screen, map, images):
-    screen.fill(BLACK)
-    for i in range(len(map)):
-        for j in range(len(map[i])):
-            image = images[map[i][j].type].get()
-            screen.blit(image, (i*16, j*16))
-    pygame.display.flip()
-            
+def update_player(screen, player, x_change, y_change, image):
+    player.x = player.x + x_change
+    player.y = player.y + y_change
+    screen.blit(image, (player.x, player.y))
     
 def main():
     pygame.init()
+#     listener = EventListener()
+#     listener.start()
     images = {}
-    for key in TILES.keys():
+    for key in IMAGES.keys():
         images[key] = Graphics(key)
-    map_image = get_map("map.png")
-    map, width, height = generate_map(map_image)
     
+    map_image = get_map("map.png")
+    map, width, height, player_start_x, player_start_y = generate_map(map_image)
+        
     screen = pygame.display.set_mode((width * 16, height * 16))
     paint_map(screen, map, images)
+    
+    player = Player(player_start_x, player_start_y)
+    update_player(screen, player, 0, 0, images["player"].get())
+
+    pygame.display.flip()
+    
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
                 sys.exit()
-
 
 if __name__ == '__main__':
     main()
